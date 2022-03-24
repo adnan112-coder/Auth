bfc0a889c0f8181957463f695e7ccf6c44c36b13
Adnan <dsa567hjj@gmail.com>
2022-03-24 12:30:39 +0530

    Add Submit route

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true})
 
 const userSchema = new mongoose.Schema ({
     email: String,
-    password: String
+    password: String,
+    secret: String
 })
 
 userSchema.plugin(passportLocalMongoose)
@@ -73,17 +74,47 @@ app.route("/login")
 
 app.route("/secrets")
 .get(function(req, res){
-    if(req.isAuthenticated()){
-        res.render("secrets")
-    }else{
-        res.redirect("/login")
-    }
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if(err){
+            console.log(err)
+        }else{
+            if(foundUsers){
+                res.render("secrets", {usersWithSecrets: foundUsers})
+            }
+        }
+    })
 })
 
 app.get("/logout", function(req, res){
     req.logout()
     res.redirect("/")
 })
+
+app.route("/submit")
+.get(function(req, res){
+     if(req.isAuthenticated()){
+        res.render("submit")
+    }else{
+        res.redirect("/login")
+    }
+})
+.post(function(req, res){
+    const submittedSecret = req.body.secret
+
+    User.findById(req.user.id, function(err, foundUser){
+        if(err){
+            console.log(err)
+        }else{
+            if(foundUser){
+                foundUser.secret = submittedSecret
+                foundUser.save(function(){
+                    res.redirect("/secrets")
+                })
+            }
+        }
+    })
+})
+
 
 app.route("/register")
 .get(function(req, res){
